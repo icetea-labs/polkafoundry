4986dd5fda36a54c87b25949429ff5b66f473c1c
Tung <thanhtung6824@gmail.com>
2021-03-11 17:19:53 +0700

    add basic test for Uniswap contract

---
+++ tests/contracts/PolkafoundryERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.6;
+
+
+// Import OpenZeppelin Contract
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// This ERC-20 contract mints the specified amount of tokens to the contract creator.
+contract PolkafoundryERC20 is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Polkafoundry", "PKF") public
+    {
+        _mint(msg.sender, initialSupply);
+    }
+}
