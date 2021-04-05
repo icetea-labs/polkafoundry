172971a875afa70fbed0dff3d1e190ffec48eff1
Tung <thanhtung6824@gmail.com>
2021-04-05 18:03:17 +0700

    update spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -104,7 +104,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("polkafoundry"),
 	impl_name: create_runtime_str!("polkafoundry"),
 	authoring_version: 1,
-	spec_version: 101,
+	spec_version: 102,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
