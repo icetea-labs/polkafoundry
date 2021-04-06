b5cd9ebbbaa88dce26f20b59ed32a4cd2ca62a44
TanMac <tan.mac@sotatek.com>
2021-04-06 11:20:05 +0700

    Feature docker build

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@
 
 TAG=$(git rev-parse --short HEAD)
 
-cargo build --release
+#cargo build --release
 docker build -f docker/Dockerfile . -t polkafoundry:$TAG
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/o0b6j5s9
