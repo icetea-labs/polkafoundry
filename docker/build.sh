b324b9568e188f81a3e9c719a45e8a76797c78e5
TanMac <tan.mac@sotatek.com>
2021-04-06 14:24:02 +0700

    Update Dockerfile for build

--- docker/build.sh
+++ docker/build.sh
@@ -5,10 +5,13 @@
 
 TAG=$(git rev-parse --short HEAD)
 
-#cargo build --release
 docker build -f docker/Dockerfile . -t polkafoundry:$TAG
+CODE=$?
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/o0b6j5s9
-
-docker tag polkafoundry:$TAG public.ecr.aws/o0b6j5s9/polkafoundry:$TAG
-docker push public.ecr.aws/o0b6j5s9/polkafoundry:$TAG
+if [ "$CODE" -eq "0" ]; then
+  aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/o0b6j5s9
+  docker tag polkafoundry:$TAG public.ecr.aws/o0b6j5s9/polkafoundry:$TAG
+  docker push public.ecr.aws/o0b6j5s9/polkafoundry:$TAG
+else
+  echo "Build image not success. exit with code: $CODE"
+fi
